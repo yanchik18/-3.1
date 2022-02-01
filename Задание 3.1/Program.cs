02b8826888f2ef32fd25bec8d9ab7f581cae0f7b
user <user@LAPTOP-FB7O3IV6>
2022-02-01 11:02:13 +0500

    Добавьте файлы проекта.

---
+++ Задание 3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Задание_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //f = ctg x + 1
+            double a, b, h, f;
+            Console.WriteLine("Введите значение a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение h");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            for (double x = a; x <= b; x += h)
+            {
+                f = Math.Tan(x) + 1;
+                Console.WriteLine(x + "/t" + f);
+            }
+        
+        }
+    }
+}
